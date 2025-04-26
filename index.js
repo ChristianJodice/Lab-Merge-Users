be3622a537b16e4d4c1db5f6f0e18f614c810149
Christian Jodice <mrjodice@gmail.com>
2025-04-26 02:07:29 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  // Loop through args and merge arrays
+  for (const array of args) {
+    combinedObject.users = [...combinedObject.users, ...array];
+  }
+
+  // Add merge date
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
